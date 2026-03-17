d13652d22db5493ad6ed01e8ad8e8461518bd08a
Utkarsh-Mauryaa <deepraj5484@gmail.com>
2026-03-18 04:57:55 +0530

    cors-fixed

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -2,6 +2,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:4173", 
+        "https://howdy-frontend-1zs1.vercel.app"
     ],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
